1f7928696494b3f340667ae1ebb78fac81375c4a
MapoDesign <elisaresmini@gmail.com>
2025-07-09 18:26:06 +0200

    logica chiamate per una corretta pipeline

--- services/db.js
+++ services/db.js
@@ -9,6 +9,26 @@ connection.connect((err) => {
   if (err) {
     console.error("Errore di connessione al database:", err);
     return;
+  } else {
+    console.log("Connessione al database riuscita!");
+    console.log(
+      "Connessione al database in corso... Così non si attiva prima del server!"
+    );
+    chiusura();
   }
-  console.log("Connessione al database riuscita!");
 });
+console.log(
+  "Connessione al database in corso... ATTENZIONE, questa operazione potrebbe arrivare prima della connessione al server!"
+);
+
+function chiusura() {
+  connection.end((err) => {
+    if (err) {
+      console.error("Errore durante la chiusura della connessione:", err);
+      return;
+    } else console.log("Connessione al database chiusa correttamente!");
+  });
+}
+
+// connection.destroy(); // Chiude la connessione al database in modo forzato
+// connection.end(); // Chiude la connessione al database in modo pulito
